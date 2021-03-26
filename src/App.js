107b1f694d7296c468136b4cdf8b7d6e416ca041
A.S. Aziz <asazizmn@gmail.com>
2021-03-26 20:18:12 +0800

    initialise git repo

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+
+
+const topics = [
+  {
+    name: 'Asian',
+    id: 'asian'
+  },
+  {
+    name: 'Turkish',
+    id: 'turkish'
+  },
+  {
+    name: 'Indian',
+    id: 'indian'
+  }
+]
+
+
+
+function Topic({ match }) {
+  const topic = topics.find(({ id }) => id === match.params.id)
+
+  return (
+    <div>
+      <h2>{topic.name}</h2>
+      <p>This is the {topic.name} description page</p>
+    </div>
+  )
+}
+
+
+function Cuisines({ match }) {
+  return (
+    <div>
+      <h1>Cuisines</h1>
+      <ul>
+        {topics.map(({ name, id }) => (
+          <li key={id}>
+            <Link to={`${match.url}/${id}`}>{name}</Link>
+          </li>
+        ))}
+      </ul>
+
+      <hr />
+
+      <Route path={`${match.path}/:id`} component={Topic} />
+    </div>
+  )
+}
+
+
+function Home() {
+  return (
+    <div>
+        <h1>Home</h1>
+        <p>This is the homepage</p>
+    </div>
+  )
+}
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/cuisines'>Cuisines</Link></li>
+          </ul>
+
+          <hr />
+
+          <Route exact path='/' component={Home} />
+          <Route path='/cuisines' component={Cuisines} />
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
+
+export default App
